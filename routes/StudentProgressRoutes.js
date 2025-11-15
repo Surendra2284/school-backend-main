35087a079915f3072123f3cf2e0acfe755c31192
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-11-15 21:19:37 +0530

    change in homewords to student progress model

--- routes/StudentProgressRoutes.js
+++ routes/StudentProgressRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const StudentProgress = require('../models/Studentprogress');
+const StudentProgress = require('../models/StudentHomework');
 const Student = require('../models/Student');
 
 // Helper: normalize date to YYYY-MM-DD

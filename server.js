026b90ce0020a6001252b7ae9c8c982f4617414c
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-11-18 19:57:05 +0530

    server1

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 // Request logger
-app.use((req, next) => {
+app.use((req, res, next) => {
   console.log(`➡️ ${req.method} ${req.path}`);
   next();
 });

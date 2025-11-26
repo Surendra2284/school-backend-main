f633ab0f2309c3d1be81ea93ff26b161081b67b8
Surendra2284 <92510811+Surendra2284@users.noreply.github.com>
2025-11-26 09:24:27 +0530

    remove userid

--- models/User.js
+++ models/User.js
@@ -4,8 +4,8 @@ const UserModel = mongoose.model("UserModel", new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     role:     {type: String, required: true},
-    isApproved: { type: Boolean, default: false },
-    Userid: {type: String,default: '0'},
+    isApproved: { type: Boolean, default: false }
+    
 }));
 
 module.exports = UserModel;
